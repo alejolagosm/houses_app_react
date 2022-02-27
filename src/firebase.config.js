fb817172da8f0026fca2d97cb7304ab474592936
alejolagosm <falame94@hotmail.com>
2022-02-26 20:40:59 -0500

    First commit of the app

---
+++ src/firebase.config.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+import { getFirestore } from 'firebase/firestore';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyA9lH1lkSx2Vhf2FBYAdFqTLZ_L1tLYUjA',
+  authDomain: 'inmovil-houses.firebaseapp.com',
+  projectId: 'inmovil-houses',
+  storageBucket: 'inmovil-houses.appspot.com',
+  messagingSenderId: '728375253520',
+  appId: '1:728375253520:web:ddefcec0b6308b15714895',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore();
